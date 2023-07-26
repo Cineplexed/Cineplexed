caa2ac4df041f56c2e7021503652f4fa3995d464
jackdevitt <jacktdevitt@gmail.com>
2023-07-26 12:12:26 -0400

    Updated sql file

--- init.sql
+++ init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Users (
+CREATE TABLE users (
     id VARCHAR(255) PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL,
@@ -10,22 +10,25 @@ CREATE TABLE Users (
     active BOOLEAN NOT NULL
 );
 
-CREATE TABLE Logs (
+CREATE TABLE logs (
     id VARCHAR(255) PRIMARY KEY,
     severity TEXT,
     content TEXT,
     timestamp TIMESTAMP
 );
 
-CREATE TABLE Guess (
-    userId VARCHAR(255) PRIMARY KEY,
-    date DATE,
-    guesses TEXT[]
-);
-
-CREATE TABLE Selections (
+CREATE TABLE selections (
     date DATE,
     movie TEXT,
-    popularity INT,
-    percent_correct INT
+    num_correct INT,
+    num_incorrect INT,
+    tagline TEXT,
+    overview TEXT,
+    genres TEXT[],
+    actors TEXT[],
+    revenue INT,
+    poster VARCHAR(255),
+    year TEXT,
+    director TEXT,
+    producer TEXT
 );
